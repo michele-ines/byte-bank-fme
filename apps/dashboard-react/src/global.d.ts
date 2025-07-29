157891c79052a51a2d4c48b0cfbb0480b1c479a9
michele-ines <minesck@hotmail.com>
2025-07-29 03:59:03 -0300

    fase-2: minha-conta

--- apps/dashboard-react/src/global.d.ts
+++ apps/dashboard-react/src/global.d.ts
@@ -3,7 +3,10 @@ declare module '*.module.scss' {
   const classes: { [key: string]: string };
   export default classes;
 }
-
+declare module '@my-cards/*' {
+  const whatever: any;
+  export default whatever;
+}
 
 declare module '*.svg' {
   const content: string;
